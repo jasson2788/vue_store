f63db899a0ab3cc7552dd78cf8bcf2d807128f61
Tim Mouskhelichvili <info@goldenmtl.com>
2019-11-02 18:49:39 -0400

    Added method to app + Updated all dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>{{ message }}</div>
+  <div>
+    <div>{{ message }}</div>
+    <button v-on:click="changeMessage()">asdasd</button>
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
     data: () => ({
+        cpt: 1,
         message: 'Hello world!!!'
-    })
+    }),
+    methods: {
+        changeMessage: function(): void {
+            this.message = `asdasdasd ${++this.cpt}`;
+        }
+    }
 });
 </script>
